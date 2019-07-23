211db1228c36a7c122720c9fe376ff7e50c521ab
Ryan Barker <celandro@gmail.com>
2019-07-23 07:56:26 -0700

    update unit tests for latest code

--- src/test/java/pogoprotos/GamemasterTest.java
+++ src/test/java/pogoprotos/GamemasterTest.java
@@ -2,10 +2,18 @@ package pogoprotos;
 
 import static org.junit.Assert.*;
 
-import java.io.InputStream;
 
 import org.junit.Test;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+
+import com.google.protobuf.util.JsonFormat;
 import POGOProtos.Networking.Responses.DownloadItemTemplatesResponseOuterClass.DownloadItemTemplatesResponse;
 
 public class GamemasterTest {
@@ -15,7 +23,13 @@ public class GamemasterTest {
 		try (InputStream is = getClass().getResourceAsStream("/GAME_MASTER.protobuf")) {
 			
 			DownloadItemTemplatesResponse response = DownloadItemTemplatesResponse.parseFrom(is);
-			assertEquals(2460, 		response.getItemTemplatesCount());
+			JsonFormat.Printer printer = JsonFormat.printer();
+			try (OutputStreamWriter writer = new OutputStreamWriter(new FileOutputStream("GAME_MASTER.json"))) {
+				printer.appendTo(response, writer);
+			}
+			
+			
+			assertEquals(3637, 		response.getItemTemplatesCount());
 		}
 	}
 
